ff2b3c2723a80150f0b399f7313eb589501621fd
Marcel Grünauer <marcel@cpan.org>
2010-02-10 14:11:18 +0000

    Import of MARCEL/Template-Plugin-GoogleChart-0.02 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-GoogleChart
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MARCEL/Template-Plugin-GoogleChart-0.02.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/GoogleChart.pm
+++ lib/Template/Plugin/GoogleChart.pm
@@ -1,25 +1,19 @@
 package Template::Plugin::GoogleChart;
-
+use 5.006;
 use strict;
 use warnings;
 use Google::Chart;
-
-
-our $VERSION = '0.01';
-
-
+our $VERSION = '0.02';
 use base qw(Template::Plugin);
-
-
-sub new_chart { Google::Chart->new }
-
-
+sub new_chart {
+    my ($self, $args) = @_;
+    Google::Chart->new($args) }
 1;
-
-
 __END__
 
-
+=for test_synopsis
+1;
+__END__
 
 =head1 NAME
 
@@ -38,7 +32,7 @@ Template::Plugin::GoogleChart - Using Google::Chart as a template plugin
 
 =over 4
 
-=item new_chart
+=item C<new_chart>
 
 Returns a new L<Google::Chart> object. See its documentation for how to use
 the chart object. You will probably want to write out the URL with the chart
@@ -47,31 +41,11 @@ shown in the synopsis.
 
 =back
 
-Template::Plugin::GoogleChart inherits from L<Template::Plugin>.
-
-The superclass L<Template::Plugin> defines these methods and functions:
-
-    new(), OLD_AUTOLOAD(), fail(), load(), old_new()
-
-The superclass L<Template::Base> defines these methods and functions:
-
-    DEBUG(), _init(), debug(), error(), module_version()
-
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<templateplugingooglechart> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.01 of L<Template::Plugin::GoogleChart>.
-
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-template-plugin-googlechart@rt.cpan.org>>, or through the web interface at
+Please report any bugs or feature requests through the web interface at
 L<http://rt.cpan.org>.
 
 =head1 INSTALLATION
@@ -82,19 +56,18 @@ See perlmodinstall for information and options on installing Perl modules.
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+site near you. Or see L<http://search.cpan.org/dist/Template-Plugin-GoogleChart/>.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+Copyright 2007-2009 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
-
 
 =cut
 
--- t/01_misc.t
+++ t/01_misc.t
@@ -1,40 +1,26 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
 use FindBin '$Bin';
 use Template;
 use Test::Differences;
 use Test::More tests => 1;
-
-
 my $template = <<EOTMPL;
 [%
     FILTER null;
         USE c = GoogleChart;
-        chart = c.new_chart;
-        chart.type_name('type_pie_3d');
-        chart.set_size(300, 300);
-        chart.data_spec({
-            encoding  => 'data_simple_encoding',
-            max_value => 100,
-            value_sets => [ [ 10, 30, 50, 70, 90 ] ],
-        });
+        chart = c.new_chart(type => 'Pie');
+        chart.pie_type('3d');
+        chart.size('300x300');
+        chart.data([ 10, 30, 50, 70, 90 ]);
     END;
-    chart.img_tag;
+    chart.as_uri;
 %]
 EOTMPL
-
-
-my $tt = Template->new(
-    INCLUDE_PATH => "$Bin/../lib",
-);
-
+my $tt = Template->new(INCLUDE_PATH => "$Bin/../lib",);
 my $result;
 $tt->process(\$template, {}, \$result) || die $tt->error();
-
 1 while chomp $result;
-
 eq_or_diff $result,
-    '<IMG SRC="http://chart.apis.google.com/chart?chs=300x300&amp;chd=s:GSeq2&amp;cht=p3" />',
-    'basic diagram';
+'http://chart.apis.google.com/chart?cht=p&chs=300x300&chd=t%3A10.0%2C30.0%2C50.0%2C70.0%2C90.0',
+  'basic diagram';
